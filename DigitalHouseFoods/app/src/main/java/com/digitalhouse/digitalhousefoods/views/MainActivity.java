cf31efd88a8303d8f231e7d04ce312ab65762140
Marvelmobile <adelania.psantos@gmail.com>
2020-02-23 17:53:34 -0300

    Adelania - Criacao Tela Registro

--- DigitalHouseFoods/app/src/main/java/com/digitalhouse/digitalhousefoods/views/MainActivity.java
+++ DigitalHouseFoods/app/src/main/java/com/digitalhouse/digitalhousefoods/views/MainActivity.java
@@ -1,8 +1,10 @@
-package com.digitalhouse.digitalhousefoods;
+package com.digitalhouse.digitalhousefoods.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.digitalhouse.digitalhousefoods.R;
 
 public class MainActivity extends AppCompatActivity {
 
