ef9b3ce13ec2bcfe9ae8c32556212055756b7169
Marvelmobile <adelania.psantos@gmail.com>
2020-02-23 19:19:19 -0300

    Adelania - Criacao Menu Profile

--- DigitalHouseFoods/app/src/main/java/com/digitalhouse/digitalhousefoods/MainActivity.java
+++ DigitalHouseFoods/app/src/main/java/com/digitalhouse/digitalhousefoods/MainActivity.java
@@ -1,8 +1,11 @@
 package com.digitalhouse.digitalhousefoods;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.menu_profile) {
+        Toast.makeText(this, "Profile", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
